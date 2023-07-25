511c5021863c692936d7e2a879903b91c97e0e3e
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2023-07-25 14:45:59 -0700

    Created New Pokemon List
    4 characters, name/heigh/type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert("Hello World!");
-let favoriteFood = "Avocado";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.5,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 2,
+        type: 'fire'
+    },
+    {
+        name: 'Squirtle',
+        height: 1.8,
+        type: 'water'
+    },
+    {
+        name: 'Weedle',
+        height: 1.0,
+        type: ['bug', 'poison']
+    }
+];
