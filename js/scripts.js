59e8f320edd590addf073a4cb47b9548ad185c2c
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2023-08-04 13:47:56 -0700

    add and getAll added

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.5,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 2,
-        type: 'fire'
-    },
-    {
-        name: 'Squirtle',
-        height: 1.8,
-        type: 'water'
-    },
-    {
-        name: 'Weedle',
-        height: 1.0,
-        type: ['bug', 'poison']
-    }
-];
 
-pokemonList.forEach(function (pokemon) {
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.5,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 2,
+            type: 'fire'
+        },
+        {
+            name: 'Squirtle',
+            height: 1.8,
+            type: 'water'
+        },
+        {
+            name: 'Weedle',
+            height: 1.0,
+            type: ['bug', 'poison']
+        }
+    ];
+
+    return {
+        add: function(newPokemon) {
+            pokemonList.push(newPokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+})();
+
+pokemonRepository.add({name: 'Pikachu', height: 1.4, type: 'Electric'});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     let heightCat;
     if (pokemon.height > 1.5) {
         heightCat = '  That is a <span> BIG </span> pokemon!';
@@ -32,3 +46,5 @@ pokemonList.forEach(function (pokemon) {
     }
     document.write('<p>' + pokemon.name + ' (height ' + pokemon.height + ') -- ' + heightCat + '</p>');
 });
+
+
