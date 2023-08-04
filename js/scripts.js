a827458b468a8401fc2fd7d8dcde9c418d4644d7
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2023-08-04 13:08:52 -0700

    Replacing for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,15 @@ let pokemonList = [
         type: ['bug', 'poison']
     }
 ];
-let pokemon = ['Bulbasaur', 'Charmander', 'Squirtle', 'Weedle'];
-let heights = [0.5, 2.0, 1.8, 1.0];
 
-for (let i = 0; i <pokemon.length; i++) {
+pokemonList.forEach(function (pokemon) {
     let heightCat;
-    if(heights[i] > 1.5) {
-        heightCat = '  That is a <span> BIG <span> pokemon!';
-    } else if (heights[i] > 0.5 && heights[i] <= 1.5) {
+    if (pokemon.height > 1.5) {
+        heightCat = '  That is a <span> BIG </span> pokemon!';
+    } else if (pokemon.height > 0.5 && pokemon.height <= 1.5) {
         heightCat = ' This is a regular size pokemon.';
     } else {
         heightCat = ' This is a small pokemon.';
     }
-document.write('<p>' + pokemon[i] + ' (height ' + heights[i] + ') -- ' + heightCat + '</p>');
-}
+    document.write('<p>' + pokemon.name + ' (height ' + pokemon.height + ') -- ' + heightCat + '</p>');
+});
